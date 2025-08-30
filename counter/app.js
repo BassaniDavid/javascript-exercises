9ed908f4e86173faabefed3c17cd543a7354d73e
BassaniDavid <davbassan98@gmail.com>
2025-08-30 15:43:10 +0200

    add color change functionality for counter and add new images for color flipper, counter, and review

--- counter/app.js
+++ counter/app.js
@@ -9,18 +9,36 @@ const increaseBtn = document.getElementById("increase");
 
 // Aggiunge la funzione per il click sul pulsante "decrease"
 decreaseBtn.addEventListener("click", function () {
-  count--; // Diminuisce il valore della variabile
-  counter.textContent = count; // Aggiorna il testo nella pagina
+  // Diminuisce il valore della variabile
+  count--;
+  if (count < 0) {
+    // Cambia il colore del testo se il contatore è negativo
+    counter.style.color = "red";
+  } else {
+    // Ripristina il colore del testo se il contatore è positivo
+    counter.style.color = "black";
+  }
+  // Aggiorna il testo nella pagina
+  counter.textContent = count;
 });
 
 // Aggiunge la funzione per il click sul pulsante "increase"
 increaseBtn.addEventListener("click", function () {
-  count++; // Aumenta il valore della variabile
-  counter.textContent = count; // Aggiorna il testo nella pagina
+  // Aumenta il valore della variabile
+  count++;
+  if (count < 0) {
+    // Cambia il colore del testo se il contatore è negativo
+    counter.style.color = "red";
+  } else {
+    // Ripristina il colore del testo se il contatore è positivo
+    counter.style.color = "black";
+  }
+  // Aumenta il valore della variabile
+  counter.textContent = count;
 });
 
 // Aggiunge la funzione per il click sul pulsante "reset"
 resetBtn.addEventListener("click", function () {
   count = 0; // Riporta il valore della variabile a 0
-  counter.textContent = count; // Aggiorna il testo nella pagina
+  counter.textContent = count;
 });
